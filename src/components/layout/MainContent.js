1f4c92cd7a84e506b0c69d525700f60c23824e84
alextran0713 <quyen137@csu.fullerton.edu>
2020-12-29 00:27:24 -0800

    vote work

--- src/components/layout/MainContent.js
+++ src/components/layout/MainContent.js
@@ -13,39 +13,50 @@ class MainContent extends Component {
     componentDidMount() {
         Axios.get("http://localhost:3001/api/posts/all").then((res) => {
             for (let i = 0; i < res.data.length; i++) {
-                console.log(res.data[i].User.name);
+                // console.log(res.data[i].updatedAt)
                 let postid = res.data[i].id;
-                Axios.get("http://localhost:3001/api/likes/" + postid).then((resp) => {
-                    // this.setState({post: res.data, likes: response.data.like.likes})
-                    Axios.get("http://localhost:3001/api/comments/" + postid).then((response) => {
-                        // this.setState({post: res.data, likes: response.data.like.likes})
-                        this.setState({
-                            post: [...this.state.post,
-                            {
-                                id: res.data[i].id,
-                                name: res.data[i].User.name,
-                                imgUrl: res.data[i].imgUrl,
-                                title: res.data[i].subject,
-                                content: res.data[i].content,
-                                likes: resp.data.like.likes,
-                                commentsCount: response.data.count
-                            }]
+                var likes = 0;
+                var counts = 0;
+                var time = res.data[i].updatedAt;
+                time = time.replace('Z', ' ').replace('T', ' ');
 
-                        })
+                Axios.get("http://localhost:3001/api/likes/" + postid).then((resp) => {
+                    if (resp.data.like.likes != null) {
+                        likes = resp.data.like.likes;
+                    }
+                    else {
+                        likes = 0;
+                    }
+                })
+                Axios.get("http://localhost:3001/api/comments/" + postid).then((response) => {
+                    counts = response.data.count;
+                    this.setState({
+                        post: [...this.state.post,
+                        {
+                            id: res.data[i].id,
+                            name: res.data[i].User.name,
+                            imgUrl: res.data[i].imgUrl,
+                            title: res.data[i].subject,
+                            content: res.data[i].content,
+                            likes: likes,
+                            commentsCount: counts
+                        }]
+
                     })
                 })
             }
         })
+
     }
 
     render() {
         return (
             <>
-            {
-                this.state.post.map((post) => (
-                    <MainPagePostCard post={post}/>
-                ))
-            }
+                {
+                    this.state.post.map((post) => (
+                        <MainPagePostCard post={post} />
+                    ))
+                }
             </>
         )
     }
--- src/components/layout/MainPagePostCard.js
+++ src/components/layout/MainPagePostCard.js
@@ -1,34 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react'
 import { MDBBtn, MDBCard, MDBCardBody, MDBCardImage, MDBCardTitle, MDBCardText, MDBCol } from 'mdbreact';
+import Axios from 'axios';
 
-const MainPagePostCard = (props) => {
-    return (
-        <div className="row rounded m-0 p-0 mb-2" style={{ background: "#fff", border: "1px solid rgba(135,138,140,0.5)"}}>
-            <div className="d-none d-md-block col-md-1 text-center py-3">
-                <ul className="list-group">
-                    <li className="list-item-post-card-like"><a><i class="far fa-caret-square-up"></i></a></li>
-                    <li className="list-item-post-card-like"><a><small><strong>{props.post.likes}</strong></small></a></li>
-                    <li className="list-item-post-card-like"><a><i class="far fa-caret-square-down"></i></a></li>
-                </ul>
-            </div>
-            <div className="col-12 col-md-11">
-                <div className="my-2"><small><strong>r/MadeMeSmile</strong> • Posted byu/{props.post.name} 3 hours ago</small></div>
-                <div>
-                    <p>{props.post.content} </p>
-                    <img className="img-fluid" src="https://picsum.photos/600/300" />
+
+export default class MainPagePostCard extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            vote: this.props.post.likes
+        }
+        this.upVotes = this.upVotes.bind(this);
+        this.downVotes = this.downVotes.bind(this);
+    }
+
+    upVotes() {
+        var newVote = this.state.vote+1;
+        this.setState({
+            vote: newVote,
+        })
+        var postid = this.props.post.id;
+        Axios.put("http://localhost:3001/api/likes/upvote/" + postid);
+    }
+
+    downVotes() {
+        var newVote = this.state.vote-1;
+        this.setState({
+            vote: newVote,
+        })
+        var postid = this.props.post.id;
+        Axios.put("http://localhost:3001/api/likes/downvote/" + postid);
+    }
+
+    render() {
+        return (
+            <div className="row rounded m-0 p-0 mb-2" style={{ background: "#fff", border: "1px solid rgba(135,138,140,0.5)" }}>
+                <div className="d-none d-md-block col-md-1 text-center py-3">
+                    <ul className="list-group">
+                        <li className="list-item-post-card-like" onClick={this.upVotes.bind(this)}><a><i class="far fa-caret-square-up"></i></a></li>
+                        <li className="list-item-post-card-like"><a><small><strong>{this.state.vote}</strong></small></a></li>
+                        <li className="list-item-post-card-like" onClick={this.downVotes.bind(this)}><a><i class="far fa-caret-square-down"></i></a></li>
+                    </ul>
+                </div>
+                <div className="col-12 col-md-11">
+                    <div className="my-2"><small><strong>r/MadeMeSmile</strong> • Posted byu/{this.props.post.name} 3 hours ago</small></div>
+                    <div>
+                        <p>{this.props.post.content} </p>
+                        <img className="img-fluid" src="https://picsum.photos/600/300" />
                     </div>
-                <div className="mt-2 text-muted">
-                    <p>
-                        <a className="mr-3"><small><i class="fas fa-comment-alt"></i> {props.post.commentsCount} Comments</small></a>
-                        <a className="mr-3"><small><i class="fas fa-share"></i> Share</small></a>
-                        <a className="mr-3"><small><i class="fas fa-bookmark"></i> Save</small></a>
-                        <a><small><i class="fas fa-ellipsis-h"></i></small></a>
-                    </p>
-                    </div>
+                    <div className="mt-2 text-muted"><p><small><i class="fas fa-comment-alt"></i>{this.props.post.commentsCount} Comments  <i class="fas fa-share"></i> Share  <i class="fas fa-bookmark"></i> Save </small></p></div>
+                </div>
             </div>
-        </div>
-        
-    )
+
+        )
+    }
 }
-
-export default MainPagePostCard;
