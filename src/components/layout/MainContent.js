3ed768a6a1ff57ecd2f49607179826f266df2c80
alextran0713 <quyen137@csu.fullerton.edu>
2020-12-28 22:18:13 -0800

    data pull

--- src/components/layout/MainContent.js
+++ src/components/layout/MainContent.js
@@ -1,11 +1,52 @@
 import React, { Component } from 'react';
 import MainPagePostCard from './MainPagePostCard';
+import Axios from 'axios';
 
 class MainContent extends Component {
-    
+    constructor(props) {
+        super(props);
+        this.state = {
+            time: null,
+            post: []
+        }
+    }
+    componentDidMount() {
+        Axios.get("http://localhost:3001/api/posts/all").then((res) => {
+            for (let i = 0; i < res.data.length; i++) {
+                console.log(res.data[i].User.name);
+                let postid = res.data[i].id;
+                Axios.get("http://localhost:3001/api/likes/" + postid).then((resp) => {
+                    // this.setState({post: res.data, likes: response.data.like.likes})
+                    Axios.get("http://localhost:3001/api/comments/" + postid).then((response) => {
+                        // this.setState({post: res.data, likes: response.data.like.likes})
+                        this.setState({
+                            post: [...this.state.post,
+                            {
+                                id: res.data[i].id,
+                                name: res.data[i].User.name,
+                                imgUrl: res.data[i].imgUrl,
+                                title: res.data[i].subject,
+                                content: res.data[i].content,
+                                likes: resp.data.like.likes,
+                                commentsCount: response.data.count
+                            }]
+
+                        })
+                    })
+                })
+            }
+        })
+    }
+
     render() {
         return (
-            <MainPagePostCard />
+            <>
+            {
+                this.state.post.map((post) => (
+                    <MainPagePostCard post={post}/>
+                ))
+            }
+            </>
         )
     }
 }
--- src/components/layout/MainPagePostCard.js
+++ src/components/layout/MainPagePostCard.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import { MDBBtn, MDBCard, MDBCardBody, MDBCardImage, MDBCardTitle, MDBCardText, MDBCol } from 'mdbreact';
 
-const MainPagePostCard = () => {
+const MainPagePostCard = (props) => {
     return (
         <div className="row rounded m-0 p-0" style={{ background: "#fff", border: "1px solid rgba(135,138,140,0.5)"}}>
             <div className="d-none d-md-block col-md-1 text-center py-3">
                 <a><i class="far fa-caret-square-up"></i></a>
-                <a><small><strong>38.9k</strong></small></a>
+                <a><small><strong>{props.post.likes}</strong></small></a>
                 <a><i class="far fa-caret-square-down"></i></a>
             </div>
             <div className="col-12 col-md-11">
-                <div className="my-2"><small><strong>r/MadeMeSmile</strong> • Posted byu/Thund3rbolt 3 hours ago</small></div>
+                <div className="my-2"><small><strong>r/MadeMeSmile</strong> • Posted byu/{props.post.name} 3 hours ago</small></div>
                 <div>
-                    <p>Etiam ac condimentum magna. Nam consectetur dui id erat dignissim fringilla. Vestibulum nisi lacus, molestie ac quam ut, lacinia molestie leo. </p>
+                    <p>{props.post.content} </p>
                     <img className="img-fluid" src="https://picsum.photos/600/300" />
                     </div>
-                <div className="mt-2 text-muted"><p><small><i class="fas fa-comment-alt"></i> 727 Comments  <i class="fas fa-share"></i> Share  <i class="fas fa-bookmark"></i> Save </small></p></div>
+                <div className="mt-2 text-muted"><p><small><i class="fas fa-comment-alt"></i>{props.post.commentsCount} Comments  <i class="fas fa-share"></i> Share  <i class="fas fa-bookmark"></i> Save </small></p></div>
             </div>
         </div>
         
